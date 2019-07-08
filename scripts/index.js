27d55cbbc886da1c5e7d95bd30b01bcc084fc1ff
superjalien <superjalien@gmail.com>
2019-07-08 23:42:01 +0800

    AR-Examples

--- scripts/index.js
+++
@@ -1,6 +0,0 @@
-Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkZWQyMmM5Ny1mNzI1LTQxNjYtYTA3Yi1jNGFhYjcyNmY3YjAiLCJpZCI6Njk4Miwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTU1MTA2NDA5Mn0.ksJ2F5hI_IDJTwa-bD-i_kVPcK6kNY_gcsgL2LPBJ0I';
-var viewer = new Cesium.Viewer('cesiumContainer', {
-    terrainProvider: Cesium.createWorldTerrain()
-});
-
-viewer._cesiumWidget._creditContainer.style.display="none";
